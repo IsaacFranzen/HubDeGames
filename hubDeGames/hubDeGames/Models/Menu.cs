20e000e6bc7eb1f36e6889e1269cac54629ca674
Isaac Franzen <isaac_lol_@outlook.com>
2023-01-19 17:40:19 -0300

    Inicia projeto - Cria a pasta Models e a classe Menu

---
+++ hubDeGames/hubDeGames/Models/Menu.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hubDeGames.Models
+{
+    public class Menu
+    {
+        public List<string> Palyers { get; set; }
+        public int options { get; set; }
+        
+        public Menu()
+        {
+            showMenu();
+        }
+
+        public void showMenu()
+        {
+            Console.WriteLine("1 - Registrar player");
+            Console.WriteLine("2 - Ver ranking de players");
+            Console.WriteLine("Digite sua opção:");
+            getOption();
+        }
+
+        public void getOption()
+        {
+            options = int.Parse(Console.ReadLine());
+        }
+    }
+}
---
+++ hubDeGames/hubDeGames/Program.cs
@@ -0,0 +1,12 @@
+﻿using hubDeGames.Models;
+
+namespace hubDeGames
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Menu menu = new Menu();
+        }
+    }
+}
